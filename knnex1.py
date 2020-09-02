d01169a76966f0da2bb40f5fbdabc97c90231f09
Rebecca Napolitano <rkn2@princeton.edu>
2020-09-02 07:37:50 -0400

    scoring

--- knnex1.py
+++ knnex1.py
@@ -5,18 +5,18 @@
 # import to conda environment
 import sklearn
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_breast_cancer
 import mglearn
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 
-
 # load dataset
-X, y = mglearn.datasets.make_forge()
+cancer = load_breast_cancer()
 
 # instantiate the class
 # test_train_split is built into sklearn. Put your cursor in the name and then press F1 for details
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=66)
 
 # set number of neighbors to use
 clf = KNeighborsClassifier(n_neighbors=3)
@@ -28,7 +28,7 @@ clf.fit(X_train, y_train)
 # this computes its nearest neighbors in the training set and finds the most common class among these:'''
 print("Test set predictions:", clf.predict(X_test))
 
-#to evaluate how well our model generalizes, we can use the score method with the test data together with the test labels:
+# to evaluate how well our model generalizes, we can use the score method with the test data together with the test labels:
 print("Test set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
 # We see that our model is about 86% accurate, meaning the model predicted the class
 # correctly for 86% of the samples in the test dataset.
@@ -45,7 +45,7 @@ for n_neighbors, ax in zip([1, 3, 9], axes):
     # and fit in one line
     clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
     mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax,
-    alpha=.4)
+                                    alpha=.4)
     mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
     ax.set_title("{} neighbor(s)".format(n_neighbors))
     ax.set_xlabel("feature 0")
@@ -62,5 +62,23 @@ where the number of neighbors is the number of all data points in the training s
 point would have exactly the same neighbors (all training points) and all predictions would
 be the same: the class that is most frequent in the training set.'''
 
-# go back to slides
+plt.figure(0)
+training_accuracy = []
+test_accuracy = []
+# try n_neighbors from 1 to 10
+neighbors_settings = range(1, 11)
+for n_neighbors in neighbors_settings:
+    # build the model
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(clf.score(X_train, y_train))
+    # record generalization accuracy
+    test_accuracy.append(clf.score(X_test, y_test))
+plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+
 
